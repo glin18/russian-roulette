645bd19e399ae8cf5de7ad8320b3bff713839b03
Chen-Hsien <zxcvb1088@gmail.com>
2023-07-09 22:14:09 +0800

    [Frontend] pass room code to backend

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`);
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+    
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
     }) ;
