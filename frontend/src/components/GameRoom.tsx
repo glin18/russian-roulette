bbd14203154eb49a5a13e8c0296c00c78ba3fe68
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-07-12 17:10:47 +0800

    Spin and fire button

--- frontend/src/components/GameRoom.tsx
+++ frontend/src/components/GameRoom.tsx
@@ -6,7 +6,12 @@ function GameRoom(props: { room: string; players: string[] }) {
       <div>GameRoom: {props.room}</div>
       <div>Round: 1</div>
       <div>Bet Pool: 0.001 ETH</div>
-      <img src={Revolver} alt="revolver" />
+      <div className="game-room-core">
+        <div className="action-button">SPIN</div>
+        <img src={Revolver} alt="revolver" />
+        <div className="action-button">FIRE</div>
+      </div>
+
       {props.players.map((player) => (
         <div key={Math.random()}>{player}</div>
       ))}
