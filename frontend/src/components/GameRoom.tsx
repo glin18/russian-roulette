680a3a9d6581df7065ff45926e41bc76efbb9321
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-07-12 17:33:59 +0800

    Rotating revolver

--- frontend/src/components/GameRoom.tsx
+++ frontend/src/components/GameRoom.tsx
@@ -1,14 +1,28 @@
 import Revolver from "../assets/revolver.png";
+import { useState } from "react";
 
 function GameRoom(props: { room: string; players: string[] }) {
+  const [rotating, setRotating] = useState<boolean>(false);
   return (
     <div className="game-room-container">
       <div>GameRoom: {props.room}</div>
       <div>Round: 1</div>
       <div>Bet Pool: 0.001 ETH</div>
       <div className="game-room-core">
-        <div className="action-button">SPIN</div>
-        <img src={Revolver} alt="revolver" />
+        <div
+          className="action-button"
+          onClick={() => {
+            setRotating(true);
+            setTimeout(() => setRotating(false), 4000);
+          }}
+        >
+          SPIN
+        </div>
+        <img
+          src={Revolver}
+          alt="revolver"
+          className={rotating ? "rotatingImage" : ""}
+        />
         <div className="action-button">FIRE</div>
       </div>
 
