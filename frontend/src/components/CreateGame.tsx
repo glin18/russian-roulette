c124875ebd824cb33abd8a59a31a09940ba15400
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-07-12 10:40:22 +0800

    Enter game room when joined room

--- frontend/src/components/CreateGame.tsx
+++ frontend/src/components/CreateGame.tsx
@@ -2,6 +2,7 @@ import createGameImage from "../assets/game.png";
 import { useAccount } from "wagmi";
 import { useState } from "react";
 import { io } from "socket.io-client";
+import GameRoom from "./GameRoom";
 
 const socket = io("http://localhost:3001");
 
@@ -15,10 +16,10 @@ function CreateGame() {
   // pass room code to backend
   const joinRoom = () => {
     setRoomCode("1234");
-    if(roomCode !== "") {
+    if (roomCode !== "") {
       socket.emit("join_room", roomCode);
     }
-  }
+  };
 
   const onClickCreate = () => {
     setShowModal(true);
@@ -30,35 +31,43 @@ function CreateGame() {
 
   return (
     <main className="outer-container">
-      <div className="page-title">YOU BET YOUR LIFE</div>
-      <div className="inner-container">
-        <div className="create-game" onClick={onClickCreate}>
-          CREATE GAME ROOM
-        </div>
-        <img src={createGameImage} alt="create game image" />
-
-        <div
-          id="myModal"
-          className="modal"
-          style={{ display: showModal ? "block" : "none" }}
-        >
-          <div className="modal-content">
-            <div className="modal-content-header">
-              <div style={{ visibility: "hidden" }}> &times;</div>
-              <div className="bet-button">Bet 0.001 ETH</div>
-              <span className="close" onClick={onClickClose}>
-                &times;
-              </span>
+      {roomCode ? (
+        <GameRoom />
+      ) : (
+        <>
+          <div className="page-title">YOU BET YOUR LIFE</div>
+          <div className="inner-container">
+            <div className="create-game" onClick={onClickCreate}>
+              CREATE GAME ROOM
             </div>
-            <div className="body-button" onClick={joinRoom}>AUTO MATCH</div>
-            <div className="body-button">INVITE WITH CODE</div>
-            <div className="room-code">
-              <input type="text" placeholder="Enter room code..." />
-              <div className="join">JOIN</div>
+            <img src={createGameImage} alt="create game image" />
+
+            <div
+              id="myModal"
+              className="modal"
+              style={{ display: showModal ? "block" : "none" }}
+            >
+              <div className="modal-content">
+                <div className="modal-content-header">
+                  <div style={{ visibility: "hidden" }}> &times;</div>
+                  <div className="bet-button">Bet 0.001 ETH</div>
+                  <span className="close" onClick={onClickClose}>
+                    &times;
+                  </span>
+                </div>
+                <div className="body-button" onClick={joinRoom}>
+                  AUTO MATCH
+                </div>
+                <div className="body-button">INVITE WITH CODE</div>
+                <div className="room-code">
+                  <input type="text" placeholder="Enter room code..." />
+                  <div className="join">JOIN</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </main>
   );
 }
---
+++ frontend/src/components/GameRoom.tsx
@@ -0,0 +1,5 @@
+function GameRoom() {
+  return <div>GameRoom</div>;
+}
+
+export default GameRoom;
